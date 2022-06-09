bd2773ba1c7937c27561e4c0f80e1f4e6169dbac
ratanovalexandr <ratanovalexandr@gmail.com>
2022-06-09 14:08:35 +0300

    add index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@
  * @param {number} maxSecond
  * @returns {number}
  */
-export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
+export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+    let minNumber = Math.min (minFirst, minSecond);
+    let maxNumber = Math.max (maxFirst, maxSecond);
+    let result = minNumber * maxNumber;
+    return result;
+};
 
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
@@ -16,7 +21,10 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
  * @param {number} radius
  * @returns {number}
  */
-export const getSquareOfCircle = (radius) => {};
+export const getSquareOfCircle = (radius) => {
+    let area = Math.PI * ((radius)*(radius));
+    return area;
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -26,7 +34,10 @@ export const getSquareOfCircle = (radius) => {};
  * @param {number} accuracy
  * @returns {number}
  */
-export const getFixedValue = (number, accuracy) => {};
+export const getFixedValue = (number, accuracy) => {
+    let num = Math.round (number * ( 10**accuracy))/ 10**accuracy;
+    return num;
+};
 
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
@@ -34,7 +45,16 @@ export const getFixedValue = (number, accuracy) => {};
  * @param {number} number
  * @returns {number}
  */
-export const getFlooroCeil = (number) => {};
+export const getFlooroCeil = (number) => {
+    if (Math.trunc(number) % 2 === 0) {
+        //Чётное
+    return Math.floor (number);
+
+    } else {
+            //Нечётное
+    return Math.ceil (number);
+    }
+};
 
 /**
  * Мы передаем два числа, от какого и до какого числа и функция должна
@@ -44,4 +64,14 @@ export const getFlooroCeil = (number) => {};
  * @param {number} to
  * @returns {number}
  */
-export const randomizer = (from, to) => {};
+export const randomizer = (from, to) => {
+            // from = Math.ceil(from);
+        // to = Math.floor(to);
+        // return Math.floor(Math.random() * (from - to)) + to; //Максимум не включается, минимум включается
+      
+
+        from = Math.ceil(from);
+        to = Math.floor(to);
+        return Math.floor(Math.random() * (from - to + 1)) + to; //Максимум и минимум включаются
+}; 
+  
